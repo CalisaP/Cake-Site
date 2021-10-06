56f9f309815c3f834790ed2f8e62069902fe2ef9
C.P <47604463+CalisaP@users.noreply.github.com>
2021-10-06 14:24:35 -0400

    Added Text Animation: Yummy
    Added Javascript trigger for a text animation in the Yummy section.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-window .onload = function(){
+window.onload = function(){
     // For header hamburger menu; source: https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
+    
+    //Selects the relevant elements
     const menu = document.querySelector(".menu");
     const menuItems = document.querySelectorAll(".menuItem");
     const hamburger = document.querySelector(".hamburger");
@@ -7,37 +9,109 @@ window .onload = function(){
     const menuIcon = document.querySelector(".menuIcon");
 
     function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
+    if (menu.classList.contains("showMenu")) { // If the menu is showing...
+        menu.classList.remove("showMenu"); // ... hide the menu,
+        closeIcon.style.display = "none"; /// ... hide the x,
+        menuIcon.style.display = "block"; /// ... show the hamburger icon.
+    } else { // Otherwise, if the menu is hidden...
+        menu.classList.add("showMenu");  // ... show the menu,
+        closeIcon.style.display = "block"; // ... show the x icon,
+        menuIcon.style.display = "none"; // ... hide the hamburger icon.
     }
     }
-    hamburger.addEventListener("click", toggleMenu);
+    hamburger.addEventListener("click", toggleMenu); // Links the toggleMenu function to the click event.
 
-
+    // Executes the toggleMenu function when any menu link is clicked.
     menuItems.forEach( 
         function(menuItem) { 
         menuItem.addEventListener("click", toggleMenu);
         }
     )
 
+    
     // For header scroll movement; source: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
-    var  prevScrollPos= window.pageYOffset;
-    window.onscroll = function(){
-        var  currentScrollPos = window.pageYOffset;
-        if (prevScrollPos > currentScrollPos){
-            document.getElementById("header").style.top = "0";
-            hamburger.style.top = ".4rem";
 
-        } else {
-            document.getElementById("header").style.top = "-60px";
-            hamburger.style.top = "-60px";
+    // Sets this variable to the page's existing offset/scroll position.
+    var  prevScrollPos= window.pageYOffset;
+
+    window.onscroll = function(){
+        var  currentScrollPos = window.pageYOffset; // Sets this variable to the pages existing offset/scroll position.
+        if (prevScrollPos > currentScrollPos){ // If the previous position is less than the current position (if you're scrolling up)...
+            document.getElementById("header").style.top = "0"; //... show the header,
+            hamburger.style.top = ".4rem"; // show the hamburger menu.
+
+        } else { // Otherwise, if you're scrolling down...
+            document.getElementById("header").style.top = "-60px"; //... hide the header
+            hamburger.style.top = "-60px"; // ... hide the hamburger menu
         }
         prevScrollPos = currentScrollPos;
+    };
+
+
+    // For scrolling movement on text; source: https://webdesign.tutsplus.com/tutorials/animate-on-scroll-with-javascript--cms-36671
+    
+    // Selects all js-scroll elements
+    const scrollElements = document.querySelectorAll(".js-scroll");
+
+
+    // Detects when the element has scrolled by the indicated amount into the page. 
+    const elementInView = (el, percentageScroll = 100) => {
+        // Gets element's distance from the top of the page.
+        const elementTop = el.getBoundingClientRect().top;
+       
+        return (
+          elementTop <= 
+          ((window.innerHeight || document.documentElement.clientHeight) * (percentageScroll/100)) //Gets height of viewport and multiplies it by the indicated amount.
+        );
+      }; 
+
+    // Assigns the .scrolled class element.
+    const displayScrollElement = (element) => {
+        element.classList.add("scrolled");
+        console.log("element should be showing");
+        console.log(element.classList.contains("scrolled"));
+    };
+    
+    // Removes the .scrolled class element.
+    const hideScrollElement = (element) => {
+        element.classList.remove("scrolled");
+        console.log("element should be hidden"); 
+        console.log(element.classList.contains("scrolled"));
+    };
+
+    // Makes the element visible/invisible depending on its position in the viewport.
+    const handleScrollAnimation = () => {
+        // For each element... 
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 100)) { //... if the element is in the viewport,
+                displayScrollElement(el); // display the element.  
+            } else { // Otherwise, if it's not in the viewport,
+                hideScrollElement(el); // hide the element.
+                
+            }
+        })
+    }
+
+    // Passes the above function to an event listener that runs whenever the user scrolls.
+    window.addEventListener("scroll", () => {
+        throttle (handleScrollAnimation, 250);
+    })
+
+    // Throttle Function
+    // Initialises the throttleTimer variable as false.
+    let throttleTimer = false;
+
+    const throttle = (callback, time) => {
+        // Doesn't run the function while throttle timer is true
+        if (throttleTimer) return;
+
+        // Sets the throttle time to true so the function doesn't run
+        throttleTimer = true;
+
+        setTimeout(() => {
+            // Calls the callback function in the setTimout and sets the throttle timer to false after the indicated time has passed.
+            callback();
+            throttleTimer = false;
+        }, time);
     }
 }
