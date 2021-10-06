462a1f920a921e89452d13a050423cdd0e1e834a
C.P <47604463+CalisaP@users.noreply.github.com>
2021-10-06 10:51:09 -0400

    Header Scroll Effect
    Added header scroll effect using javascript.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window .onload = function(){
-    // For header hamburger menu
+    // For header hamburger menu; source: https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
     const menu = document.querySelector(".menu");
     const menuItems = document.querySelectorAll(".menuItem");
     const hamburger = document.querySelector(".hamburger");
@@ -25,4 +25,19 @@ window .onload = function(){
         menuItem.addEventListener("click", toggleMenu);
         }
     )
+
+    // For header scroll movement; source: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+    var  prevScrollPos= window.pageYOffset;
+    window.onscroll = function(){
+        var  currentScrollPos = window.pageYOffset;
+        if (prevScrollPos > currentScrollPos){
+            document.getElementById("header").style.top = "0";
+            document.getElementById("hamburgerMenu").style.top = ".4rem";
+
+        } else {
+            document.getElementById("header").style.top = "-60px";
+            document.getElementById("hamburgerMenu").style.top = "-60px";
+        }
+        prevScrollPos = currentScrollPos;
+    }
 }
