f46fe15540e252ca8dcb85e935913631b18b4c60
C.P <47604463+CalisaP@users.noreply.github.com>
2021-10-06 15:39:50 -0400

    Replaced Hamburger Menu Icon
    Replaced hamburger menu icon with a cake icon. Also moved social media icons to new icon folder.

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ window.onload = function(){
     const menu = document.querySelector(".menu");
     const menuItems = document.querySelectorAll(".menuItem");
     const hamburger = document.querySelector(".hamburger");
+    const cakeIcon = document.querySelector(".cake-figure")
     const closeIcon = document.querySelector(".closeIcon");
     const menuIcon = document.querySelector(".menuIcon");
 
     function toggleMenu() {
     if (menu.classList.contains("showMenu")) { // If the menu is showing...
         menu.classList.remove("showMenu"); // ... hide the menu,
-        closeIcon.style.display = "none"; /// ... hide the x,
-        menuIcon.style.display = "block"; /// ... show the hamburger icon.
+        closeIcon.style.display = "none"; // ... hide the x,
+        //menuIcon.style.display = "block"; // ... show the hamburger icon.
+        cakeIcon.style.display = "block"; // ... show the cake icon.
     } else { // Otherwise, if the menu is hidden...
         menu.classList.add("showMenu");  // ... show the menu,
         closeIcon.style.display = "block"; // ... show the x icon,
-        menuIcon.style.display = "none"; // ... hide the hamburger icon.
+        //menuIcon.style.display = "none"; // ... hide the hamburger icon.
+        cakeIcon.style.display = "none"; //v ... hide the cake icon.
     }
     }
     hamburger.addEventListener("click", toggleMenu); // Links the toggleMenu function to the click event.
