c68561a526708d58c26a7588936c8d2930e05830
C.P <47604463+CalisaP@users.noreply.github.com>
2021-12-03 15:40:03 -0400

    Coordinated Cake Movements
    Wrote loop to coordinate simultaneous cake movements.

--- menu-page-script.js
+++ menu-page-script.js
@@ -6,7 +6,45 @@ window.addEventListener("load", (event) => {
     const cakeTwoCaption = document.getElementById("cake-2-caption");
     const cakeThree = document.getElementById("cake-figure-3");
     const cakeThreeCaption = document.getElementById("cake-3-caption");
+    const cakes = document.getElementsByClassName("cake-img");
     const sliderRightArrow = document.getElementById("slider-right-arrow");
+
+    function left(cake){
+        cake.classList.remove("rotate-cake");
+        cake.classList.add("rotate-cake-left");
+    }
+    function right(cake){
+        cake.classList.remove("rotate-cake-left");
+        cake.classList.add("rotate-cake-right");
+        cake.style.left = "-33.5rem";
+        cake.style.top = "-8rem";
+    }
+    function front(cake){
+        cake.classList.remove("rotate-cake-right");
+        cake.classList.add("rotate-cake-front");
+        cake.style.left = "33rem";
+    }
+    function home(cake){
+        cake.classList.remove("rotate-cake-front");
+        cake.classList.add("rotate-cake-left");
+        cake.style.left = "0rem";
+        cake.style.top = "0rem";
+    }
+    function clockwiseRotations(i){
+        for (let i = 0; i < cakes.length; i++){
+            console.log(cakes[i]);
+            if (cakes[i].classList.contains("rotate-cake")){
+                left(cakes[i]);
+            } else if (cakes[i].classList.contains("rotate-cake-left")){
+                right(cakes[i]);
+            } else if (cakes[i].classList.contains("rotate-cake-right")){
+                front(cakes[i]);
+            } else if (cakes[i].classList.contains("rotate-cake-front")){
+                home(cakes[i]);
+            }
+        }
+    }
+    
 
     function clockwiseRotation(){
         if (cakeTwo.classList.contains("rotate-cake")){
@@ -41,6 +79,6 @@ window.addEventListener("load", (event) => {
         }
     }        
     sliderRightArrow.addEventListener("click", () => {
-        clockwiseRotation();
+        clockwiseRotations();
     });
 });
