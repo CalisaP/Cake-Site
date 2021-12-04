2d194d523fd2620dc38802987c625d6541363e47
C.P <47604463+CalisaP@users.noreply.github.com>
2021-12-04 15:30:51 -0400

    Animated Cake 1 Movement

--- menu-page-script.js
+++ menu-page-script.js
@@ -1,46 +1,109 @@
 window.addEventListener("load", (event) => {
     // FOR MENU PAGE CAKE ROTATION
-    const cakeOne = document.getElementById("cake-figure-1");
+    const cakeOne = document.getElementById("cake-1");
     const cakeOneCaption = document.getElementById("cake-1-caption");
     const cakeTwo = document.getElementById("cake-2");
     const cakeTwoCaption = document.getElementById("cake-2-caption");
-    const cakeThree = document.getElementById("cake-figure-3");
+    const cakeThree = document.getElementById("cake-3");
     const cakeThreeCaption = document.getElementById("cake-3-caption");
     const cakes = document.getElementsByClassName("cake-img");
+    const captions = document.getElementsByClassName("cake-caption");
     const sliderRightArrow = document.getElementById("slider-right-arrow");
 
-    function left(cake){
-        cake.classList.remove("rotate-cake");
-        cake.classList.add("rotate-cake-left");
+    function cakeOneClockwise(cake, caption){
+        if (cake.classList.contains("rotate-cake")){
+            cake.classList.remove("rotate-cake");
+            caption.classList.remove("rotate-cake");
+            cake.classList.add("rotate-cake-1-right");
+            caption.classList.add("rotate-cake-caption-1-right");
+                console.log("going right");
+        } else if (cake.classList.contains("rotate-cake-1-right")){
+            cake.classList.remove("rotate-cake-1-right");
+            caption.classList.remove("rotate-cake-caption-1-right");    
+            cake.classList.add("rotate-cake-1-front");
+            caption.classList.add("rotate-cake-caption-1-front");
+            cake.style.left = "33.5rem";
+            caption.style.left = "66.5rem";
+                console.log("going front");
+        } else if (cake.classList.contains("rotate-cake-1-front")){
+            cake.classList.remove("rotate-cake-1-front");
+            caption.classList.remove("rotate-cake-caption-1-front");    
+            cake.classList.add("rotate-cake-1-left");
+            caption.classList.add("rotate-cake-caption-1-left");
+            cake.style.left = "0rem";
+            cake.style.top = "8rem";
+            caption.style.left = "33rem";
+            caption.style.top = "8rem";
+                console.log("going left");
+        } else if (cake.classList.contains("rotate-cake-1-left")){
+            cake.classList.remove("rotate-cake-1-left");
+            caption.classList.remove("rotate-cake-caption-1-left");    
+            cake.classList.add("rotate-cake-1-right");
+            caption.classList.add("rotate-cake-caption-1-right");
+            cake.style.left = "-30rem";
+            cake.style.top = "-.5rem";
+            caption.style.left = ".5rem";
+            caption.style.top = "0rem";                
+            console.log("going home");
+        }
     }
-    function right(cake){
-        cake.classList.remove("rotate-cake-left");
-        cake.classList.add("rotate-cake-right");
-        cake.style.left = "-33.5rem";
-        cake.style.top = "-8rem";
+    function cakeTwoClockwise(cake, caption){
+        if (cake.classList.contains("rotate-cake")){
+            cake.classList.remove("rotate-cake");
+            caption.classList.remove("rotate-cake");
+            cake.classList.add("rotate-cake-2-left");
+            caption.classList.add("rotate-cake-2-left");
+            } else if (cake.classList.contains("rotate-cake-2-left")){
+            cake.classList.remove("rotate-cake-2-left");
+            caption.classList.remove("rotate-cake-2-left");
+            cake.classList.add("rotate-cake-2-right");
+            caption.classList.add("rotate-cake-2-right");
+            cake.style.left = "-33.5rem";
+            cake.style.top = "-8rem";
+            caption.style.left = "-33.5rem";
+            caption.style.top = "-8rem";
+            } else if (cake.classList.contains("rotate-cake-2-right")){
+            cake.classList.remove("rotate-cake-2-right");
+            caption.classList.remove("rotate-cake-2-right");    
+            caption.classList.add("rotate-cake-2-front");
+            cake.classList.add("rotate-cake-2-front");
+            cake.style.left = "30rem";
+            caption.style.left = "30rem";
+            } else if (cake.classList.contains("rotate-cake-2-front")){
+            cake.classList.remove("rotate-cake-2-front");
+            caption.classList.remove("rotate-cake-2-front");    
+            cake.classList.add("rotate-cake-2-left");
+            caption.classList.add("rotate-cake-2-left");
+            cake.style.left = "0rem";
+            cake.style.top = "0rem";
+            caption.style.left = "0rem";
+            caption.style.top = "0rem";
+            }
     }
-    function front(cake){
-        cake.classList.remove("rotate-cake-right");
-        cake.classList.add("rotate-cake-front");
-        cake.style.left = "33rem";
-    }
-    function home(cake){
-        cake.classList.remove("rotate-cake-front");
-        cake.classList.add("rotate-cake-left");
-        cake.style.left = "0rem";
-        cake.style.top = "0rem";
-    }
-    function clockwiseRotations(i){
+
+    function clockwiseCakeRotations(i){
         for (let i = 0; i < cakes.length; i++){
-            console.log(cakes[i]);
             if (cakes[i].classList.contains("rotate-cake")){
-                left(cakes[i]);
+                cakeLeft(cakes[i]);
             } else if (cakes[i].classList.contains("rotate-cake-left")){
-                right(cakes[i]);
+                cakeRight(cakes[i]);
             } else if (cakes[i].classList.contains("rotate-cake-right")){
-                front(cakes[i]);
+                cakeFront(cakes[i]);
             } else if (cakes[i].classList.contains("rotate-cake-front")){
-                home(cakes[i]);
+                cakeHome(cakes[i]);
+            }
+        }
+    }
+    function clockwiseCaptionsRotations(i){
+        for (let i = 0; i < captions.length; i++){
+            if (captions[i].classList.contains("rotate-cake")){
+                captionLeft(captions[i]);
+            } else if (captions[i].classList.contains("rotate-cake-left")){
+                captionRight(captions[i]);
+            } else if (captions[i].classList.contains("rotate-cake-right")){
+                captionFront(captions[i]);
+            } else if (captions[i].classList.contains("rotate-cake-front")){
+                captionHome(captions[i]);
             }
         }
     }
@@ -79,6 +142,7 @@ window.addEventListener("load", (event) => {
         }
     }        
     sliderRightArrow.addEventListener("click", () => {
-        clockwiseRotations();
+        cakeOneClockwise(cakeOne, cakeOneCaption);
+        cakeTwoClockwise(cakeTwo, cakeTwoCaption);
     });
 });
